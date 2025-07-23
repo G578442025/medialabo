48917fce44a69166bafda461d86b0cacdff8b592
G578442025 <257844@st.takushoku-u.ac.jp>
2025-07-23 15:29:02 +0900

    完

--- tv-program.js
+++ tv-program.js
@@ -18,30 +18,32 @@ function print(data) {
   });
 }
 
+let btn = document.querySelector('button');
+btn.addEventListener('click', sendRequest);
 
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
-  const oldResult = document.querySelector("#result");
+  let oldResult = document.querySelector("#result");
   if (oldResult) {
     oldResult.remove();
 }
 
-const resultDiv = document.createElement("div");
+let resultDiv = document.createElement("div");
   resultDiv.id = "result";
   resultDiv.classList.add("result-box"); // CSSが適用されるように
 
   // 見出しを追加
-  const title = document.createElement("div");
+  let title = document.createElement("div");
   title.className = "result-title";
   title.textContent = "番組表の検索結果";
   resultDiv.appendChild(title);
 
   // table 要素を作成
-  const table = document.createElement("table");
+  let table = document.createElement("table");
 
   // thead を作成
-  const thead = document.createElement("thead");
+  let thead = document.createElement("thead");
   thead.innerHTML = `
     <tr>
       <th>開始時刻</th>
@@ -54,29 +56,36 @@ const resultDiv = document.createElement("div");
   table.appendChild(thead);
 
   // tbody を作成
-  const tbody = document.createElement("tbody");
+  let tbody = document.createElement("tbody");
+  let programs;
+  if(data.list!==null ){
 
-  const programs = data.list.g1;
+  if(data.list.e1!==undefined){
+    programs=data.list.e1
+  }else{
+    programs = data.list.g1
+  }
+  
   programs.forEach(program => {
     const tr = document.createElement("tr");
 
-    const tdStart = document.createElement("td");
+    let tdStart = document.createElement("td");
     tdStart.textContent = program.start_time;
     tr.appendChild(tdStart);
 
-    const tdEnd = document.createElement("td");
+    let tdEnd = document.createElement("td");
     tdEnd.textContent = program.end_time;
     tr.appendChild(tdEnd);
 
-    const tdChannel = document.createElement("td");
+    let tdChannel = document.createElement("td");
     tdChannel.textContent = program.service.name;
     tr.appendChild(tdChannel);
 
-    const tdTitle = document.createElement("td");
+    let tdTitle = document.createElement("td");
     tdTitle.textContent = program.title;
     tr.appendChild(tdTitle);
 
-    const tdAct = document.createElement("td");
+    let tdAct = document.createElement("td");
     tdAct.textContent = program.act || "記載なし";
     tr.appendChild(tdAct);
 
@@ -87,7 +96,13 @@ const resultDiv = document.createElement("div");
   resultDiv.appendChild(table);
 
   // body の末尾に追加
-  document.body.appendChild(resultDiv);
+  
+}else{
+ let p = document.createElement("p");
+ p.textContent = "チャンネルがありませんでした"
+ resultDiv.insertAdjacentElement("beforeend",p);
+}
+document.body.appendChild(resultDiv);
 }
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
@@ -97,10 +112,26 @@ const resultDiv = document.createElement("div");
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
 
+let s = document.querySelector('select#ch1');
+
+let r = document.querySelector('select#ch2');
+
+
+  let url = "https://www.nishita-lab.org/web-contents/jsons/nhk/"+s.value+"-"+r.value+"-j.json";
+
+  axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+        data = JSON.parse(data);
+  }
+  printDom(data);
 
 }
 
@@ -114,88 +145,88 @@ function finish() {
     console.log('Ajax 通信が終わりました');
 }
 
-////////////////////////////////////////
+// //////////////////////////////////////
 // 以下はテレビ番組表のデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
-  }
-};
+// let data = {
+// "list": {
+//     "g1": [
+//       {
+//         "id": "2022030428673",
+//         "event_id": "28673",
+//         "start_time": "2022-03-04T04:35:00+09:00",
+//         "end_time": "2022-03-04T04:40:00+09:00",
+//         "area": {
+//           "id": "130",
+//           "name": "東京"
+//         },
+//         "service": {
+//           "id": "g1",
+//           "name": "ＮＨＫ総合１",
+//           "logo_s": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
+//             "width": "100",
+//             "height": "50"
+//           },
+//           "logo_m": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
+//             "width": "200",
+//             "height": "100"
+//           },
+//           "logo_l": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
+//             "width": "200",
+//             "height": "200"
+//           }
+//         },
+//         "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
+//         "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
+//         "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
+//         "act": "",
+//         "genres": [
+//           "0409",
+//           "0700",
+//           "0504"
+//         ]
+//       },
+//       {
+//         "id": "2022030427069",
+//         "event_id": "27069",
+//         "start_time": "2022-03-04T23:05:00+09:00",
+//         "end_time": "2022-03-04T23:10:00+09:00",
+//         "area": {
+//           "id": "130",
+//           "name": "東京"
+//         },
+//         "service": {
+//           "id": "g1",
+//           "name": "ＮＨＫ総合１",
+//           "logo_s": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
+//             "width": "100",
+//             "height": "50"
+//           },
+//           "logo_m": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
+//             "width": "200",
+//             "height": "100"
+//           },
+//           "logo_l": {
+//             "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
+//             "width": "200",
+//             "height": "200"
+//           }
+//         },
+//         "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
+//         "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
+//         "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
+//         "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
+//         "genres": [
+//           "0700"
+//         ]
+//       }
+//     ]
+//   }
+// };
 
