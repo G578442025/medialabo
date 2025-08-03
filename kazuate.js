3921fcdf441dc4bbfc86eab604055c78e421baf2
G578442025 <257844@st.takushoku-u.ac.jp>
2025-08-03 20:14:54 +0900

    完了

--- kazuate.js
+++ kazuate.js
@@ -1,34 +1,34 @@
 // 答え
-let kotae = Math.floor(Math.random()*10) + 1;
+let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
+// 回答回数
 let kaisu = 1;
 document.getElementById("kaisu").textContent = kaisu;
 
-document.getElementById("kaitou").addEventListener("click",hantei);
+document.getElementById("kaitou").addEventListener("click", hantei);
 
 function hantei() {
+  let yoso = Number(document.getElementById("yoso").value);
+  let result = document.getElementById("result");
+  let answer = document.getElementById("answer");
 
+  answer.textContent = yoso;
 
-  let yoso1 = document.querySelector("input#yoso");
-   yoso1.Value;
-  let yoso = yoso1;
-  kaisu=kaisu+1;
-  console.log(kaisu+"回目の予想:"+yoso);
-
-if(kaisu>=4){
-  console.log("答えは"+kotae+"でした。すでにゲームは終わっています");
-}else if(kotae===yoso){
-  console.log("正解です。おめでとう！");
-}else{
-  if(kaisu===3){
-    console.log("まちがい、残念でした答えは"+kotae+"です。")
-  }else if(kaisu>=2&&yoso<kotae){
-    console.log("まちがい、答えはもっと大きいですよ")
-  }else if(kaisu>=2&&yoso>kotae){
-    console.log("まちがい、答えはもっと小さいですよ")
+  if (kaisu <= 3) {
+    if (yoso === kotae) {
+      console.log("正解です。おめでとう！");
+    } else if (kaisu === 3) {
+      console.log("まちがい。残念でした。答えは " + kotae + " です。");
+    } else if (yoso > kotae) {
+      console.log("まちがい。答えはもっと小さいですよ。");
+    } else if (yoso < kotae) {
+      console.log("まちがい。答えはもっと大きいですよ。");
+    }
+  } else {
+    console.log("答えは " + kotae + " でした。すでにゲームは終わっています。");
   }
-}
 
+  kaisu++;
+  document.getElementById("kaisu").textContent = kaisu;
 }
